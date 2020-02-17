4a0b6ae600c00cca7c9f380301c87508a93cfc40
Zhanna <j.santybay@gmail.com>
2020-02-17 16:42:23 +0100

    demande de mission #1

--- src/main/java/dev/repository/MissionRepo.java
+++ src/main/java/dev/repository/MissionRepo.java
@@ -21,7 +21,7 @@ public interface MissionRepo extends JpaRepository<Mission, Long> {
 	@Query("SELECT m FROM Mission m, Collegue c WHERE m.collegue.id=c.id AND c.id=:id")
 	List<Mission> findByCollegueId(Long id);
 
-	@Query("SELECT m FROM Mission m WHERE m.collegue.id=:id AND m.date_debut=:dateDebut AND m.date_fin=:dateFin")
+	@Query("SELECT m FROM Mission m WHERE m.collegue.id=:id AND m.dateDebut=:dateDebut AND m.dateFin=:dateFin")
 	Optional<Collegue> findByCollegueIdDateDebutDateFin(Long id, LocalDate dateDebut, LocalDate dateFin);
 
 }
